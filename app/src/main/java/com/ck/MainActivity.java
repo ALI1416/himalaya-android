d3ef49b9380c8fd8529f4e5c63ecb2bb81af44a7
ALI <1416978277@qq.com>
2020-09-12 22:16:50 +0800

    add new file

--- app/src/main/java/com/ck/MainActivity.java
+++ app/src/main/java/com/ck/MainActivity.java
@@ -1,14 +1,27 @@
 package com.ck;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private final MainActivity _this = this;
+
+    private void log(String msg) {
+        Log.d(TAG, msg);
+    }
+
+    private void toast(String msg) {
+        Toast.makeText(_this, msg, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/com/ck/base/BaseApplication.java
@@ -0,0 +1,26 @@
+package com.ck.base;
+
+import android.app.Application;
+
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        /*设置AppKey等*/
+        CommonRequest mXimalaya = CommonRequest.getInstanse();
+        if (DTransferConstants.isRelease) {
+            String mAppSecret = "afe063d2e6df361bc9f1fb8bb8210d67";
+            mXimalaya.setAppkey("af1d317b871e0e7e2ce45872caa34d9a");
+            mXimalaya.setPackid("com.humaxdigital.automotive.ximalaya");
+            mXimalaya.init(this, mAppSecret);
+        } else {
+            String mAppSecret = "0a09d7093bff3d4947a5c4da0125972e";
+            mXimalaya.setAppkey("f4d8f65918d9878e1702d49a8cdf0183");
+            mXimalaya.setPackid("com.ximalaya.qunfeng");
+            mXimalaya.init(this, mAppSecret);
+        }
+    }
+}
